9cbf75706e4b7fb1951afc379e9e942807f4bc80
Quant85 <ant.quattrocchi@stud.uniroma3.it>
2021-05-25 17:18:39 +0200

    ciclo richiesta-risposta, funzioni middleware

--- node-express/app.js
+++ node-express/app.js
@@ -11,36 +11,63 @@ app.enable('case sensitive routing'); // in questo modo abbiamo abilitato l'impo
 app.disable('x-powered-by');
 
 
-app.get('/blog',(req,res) => {
-  console.log(req.query);
-  res.send('ok\n');
+
+// Funzioni middleware
+//http://expressjs.com/it/guide/using-middleware.html#utilizzo-del-middleware
+
+/**
+ * Express è un framework Web di routing e middleware, con funzionalità sua propria minima: 
+ * un’applicazione Express è essenzialmente a serie di chiamate a funzioni middleware.
+ */
+
+app.get('/', prima, seconda, terza);
+
+//definiamo le funzioni middleware esternamente
+
+/** Cosa può fare una funzione middleware?
+ * Può eseguire qualsiasi codice necessario
+ * Può terminare il ciclo richiesta risposta
+ * Può invocaree la successiva funzione middleware
+ * Può modificare l'oggetto response request
+ */
+
+function prima(req, res, next) {
+  console.log('Nuova richiesta ricevuta dalla prima middleware');
+  next();
+};
+
+function seconda(req, res, next) {
+  console.log('Nuova richiesta ricevuta dalla seconda middleware');
+  next();
+};
+
+function terza(req, res) {
+  console.log('Nuova richiesta ricevuta dalla terza middleware');
+  res.send('ok\n');// termina il cilco richiesta-risposta
+};
+
+
+//------
+
+app.get('/user/risorsa-premium', checkAuthentication, checkAuthorization, (req, res) => {
+  res.send('Ecco a te la risorsa premium...\n');// termina il cilco
+  //contiene tre funzioni middleware, l'ultima definita direttamente
 });
 
-//http://localhost:3000/blog?anno=2020&mese=8
-// { anno: '2020', mese: '8' }
+function checkAuthentication(req,res,next) { 
+  const isLogged = true; // false - non loggato, true loggato
+  if(!isLogged) return res.status(401).send('Non sei autenticato\n'); 
+  //termina il ciclo
+  //modificare l'oggeto req o res ci permette di passare delle informazioni da una funzione middleware ad un'altra
+  req.user = { nome: 'Sara', tipo: 'Standard' }; // informazione passata all'altra funzione middleware - Premium
+  next();
+}
 
 
-// app.get('/blog/:anno/:mese',(req,res) => {
-//   console.log(req.params);
-//   res.send('ok\n');
-// });
-
-//http://localhost:3000/blog/2020/8
-// { anno: '2020', mese: '8' }
-
-//Possiamo anche combianre i due metodi
-app.get('/blog/:anno/:mese',(req,res) => {
-  console.log(req.params);
-  console.log(req.query);
-  res.send('ok\n');
-});
-
-//http://localhost:3000/blog/2020/8?ordinaPer=titolo
-
-// [nodemon] starting `node app.js`
-// { anno: '2000', mese: '8' } //parametri di rotta che vanno conosciuti per poter ottenere una risposta dal client
-// { ordinaPer: 'titolo' } // sintassi classica per informazioni non necessarie a fin che otteniamo una risposta dal client
-
-
+function checkAuthorization(req,res,next) { 
+  const isAutorizzato = req.user.tipo === 'Premium'? true : false;
+  if(!isAutorizzato) return res.status(403).send('Non sei autorizzato\n');
+  next();
+}
 
 app.listen(3000);
