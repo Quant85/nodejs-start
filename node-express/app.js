5a249102c24a8f7afa9a8cf25181d5fc4f1bf3f2
Quant85 <ant.quattrocchi@stud.uniroma3.it>
2021-05-25 11:38:21 +0200

    routing all- esempio logging 401

--- node-express/app.js
+++ node-express/app.js
@@ -3,32 +3,11 @@ const express = require('express');
 const app = express();
 
 //routing
-
-app.get('/', (req, resp) => {
-  console.log('Nuova richiesta GET alla rotta root');
-  resp.send("<h1>Benvenuto</h1>\n")
-}); // per ottenere una risorsa
-
-app.post('/', (req, resp) => {
-  console.log('Nuova richiesta POST alla rotta root');
-  resp.send("<h1>Benvenuto</h1>\n")
-});// per creare una nuova risorsa
-
-app.put('/', (req, resp) => {
-  console.log('Nuova richiesta PUT alla rotta root');
-  resp.send("<h1>Benvenuto</h1>\n")
-});//per aggiornarla
-
-app.delete('/', (req, resp) => {
-  console.log('Nuova richiesta DELETE alla rotta root');
-  resp.send("<h1>Benvenuto</h1>\n")
-});// per eliminarla
-// con express utiliziamo send al posnto di end per inviare una risposta
-
-app.get('/blog', (req, resp) => {
-  console.log('nuova richiesta GET alla rotta /blog');
-  resp.send("<h1>Benvenuto nel blog</h1>")
+app.all('/api/*', (req, res) => {
+  const isLogged = false;
+  if (!isLogged) {
+    res.status(401).send('Non sei loggato\n')
+  }
 });
-
 
 app.listen(3000);
