06544c8bd30b16025f456df0a8eed55f32203b16
Quant85 <ant.quattrocchi@stud.uniroma3.it>
2021-05-28 17:23:23 +0200

    pulizia app.js

--- node-express/app.js
+++ node-express/app.js
@@ -1,34 +1,23 @@
 // --- Invocazione dei vari moduli
-const fs = require('fs');
-
-const morgan = require('morgan');
-const helmet = require('helmet');
-
 const express = require('express');
-
-const { userAuth, userPerms } = require('./middleware/user-auth');
 
 // le cinque sotto-applicazioni
 const homeRouter = require('./routes/home');
 const userRouter = require('./routes/user');
 const blogRouter = require('./routes/blog');
-const contattiRouter = require('./routes/contatti');
 const _404Router = require('./routes/404');
+const { userAuth, userPerms } = require('./middleware/user-auth');
 
 const app = express(); //creiamo l'oggetto app
-const log =fs.createWriteStream('logs/access.log', { flags: 'a'}); //creiamo lo stream scrivibile per il file access.log
 
 
 //http://expressjs.com/it/api.html#app.set
 
-app.use(morgan('combined', { stream: log })); //attiviamo morgan
-app.use(helmet());// attiviamo helmet
 app.use('/user', userAuth, userPerms); // cosi stiamo chiedendo che ad ogni richiesta che viene effettuata di applicare le funzioni userAuth e userPerms
 
 app.use(homeRouter);
 app.use(userRouter);
-app.use(blogRouter);
-app.use(contattiRouter);
+app.use('/blog',blogRouter);
 app.use(_404Router);
 
 app.listen(3000);
--- node-express/routes/blog.js
+++ node-express/routes/blog.js
@@ -3,11 +3,11 @@ const express = require('express');
 const router = express.Router(); //creiamo una nuova istanza di tipo Router
 
 
-router.get('/blog/articoli', (req, res) => {
+router.get('/articoli', (req, res) => {
   res.send('Pagina articoli\n');
 });
 
-router.get('/blog/articoli/:titolo', (req, res) => {
+router.get('/articoli/:titolo', (req, res) => {
   res.send('Pagina articoli\n');
 });
 
