c76d275ddeca7544f28a60686aaf453586dc688d
Quant85 <ant.quattrocchi@stud.uniroma3.it>
2021-06-01 10:54:11 +0200

    predisposizione variabile db document collection

--- node-express/node-express-mongodb/app.js
+++ node-express/node-express-mongodb/app.js
@@ -1,11 +1,32 @@
-
+const express = require('express');
 const { MongoClient } = require('mongodb');
+
+const app = express();
+
 const dbURI = 'mongodb+srv://antonio:mdb123@prova-node.wbyji.mongodb.net/blog?retryWrites=true&w=majority';
 
 const mongoClient = new MongoClient(dbURI, { useUnifiedTopology: true });
 
+let blogDB, articoliCollection;
+
+//Creare 
+app.get('/nuovo-articolo', (req,res) => {});
+
+//Read
+app.get('/articolo', (req,res) => {});
+
+//Update
+app.get('/modifica-articolo', (req,res) => {});
+
+//Delete
+app.get('/cancella-articolo', (req,res) => {});
+
+
 async function run() { 
   await mongoClient.connect();
   console.log('Siamo connessi a MongoDB Atlas!');
+  app.listen(8000, () => console.log('Server in ascolto sulla porta 8000 ...'));
+  blogDB = mongoClient.db('blog');
+  articoliCollection = blogDB.collection('articoli');
 }
 run().catch(err => console.log('Errore connessione: ' + err));
