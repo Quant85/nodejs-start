684d8fb97d7eed248489818ce3f92eb2962c9308
Quant85 <ant.quattrocchi@stud.uniroma3.it>
2021-06-04 19:30:13 +0200

    pull - con callback : { tags: { : 9 } }

--- node-express-mongodb/app.js
+++ node-express-mongodb/app.js
@@ -14,7 +14,7 @@ app.get('/', async (req, res) => {
   // incrementiamo il campo voto con l'operatore $inc
   const filtro = { _id: ObjectID('60b5f6ba89e19f6f0943ba0e') };
   // cosa vogliamo incrementare e di quanto
-  const update = { $pull: { tags: "javascript" } };
+  const update = { $pull: { tags: { $lt: 9 } } };
   await articoliCollection.updateOne(filtro, update);
 
   articoliCollection.findOne({}).then(articolo => {
